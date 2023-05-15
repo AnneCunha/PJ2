c2451c874d4fc9b3a577ed005f973f586c258ddf
LuizzFellipe <luiz.fellipe@aluno.ifsp.edu.br>
2023-05-15 17:11:06 -0300

    Atualização dos elementos

--- PJ2/js/doacaoMonetaria.js
+++ PJ2/js/doacaoMonetaria.js
@@ -1,13 +1,3 @@
-const inputMesAno = document.getElementById('dateValido');
-const valor = inputMesAno.value;
-const regex = /^\d{2}\/\d{4}$/; // Expressão regular para formato "MM/AAAA" com números
+// Select Benificente ou Requerente
 
-if (regex.test(valor)) {
-  const ano = valor.substring(4); // Obtém os últimos 4 caracteres (ano)
-  const mes = valor.substring(0, 2); // Obtém os primeiros 2 caracteres (mês)
-
-  console.log('Ano: ' + ano); // Exibe o ano no console
-  console.log('Mês: ' + mes); // Exibe o mês no console
-} else {
-  console.log('Formato inválido'); // Exibe mensagem de erro no console
-}
+const   
